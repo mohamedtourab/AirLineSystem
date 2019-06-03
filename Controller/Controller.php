ee389a7294f9b76d1a9d24865967b3f40601985b
Mohamed Mamdouh <33672420+MohamedMamdouh95@users.noreply.github.com>
2019-06-03 20:27:10 +0200

    tested select,update,insert functions in the Model

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -1,22 +1,35 @@
 <?php
 require_once('../Model/Model.php');
 
-$myModel = new Model("mohamed","mohamed","airlinedatabase");
-$postUserName = $_POST['userName'];
-$postPassword = $_POST['password'];
-    $myModel->insert($postUserName,$postPassword);
 
 
-/*class Controller
+class Controller
 {
-    protected $myModel;
-    function __construct()
+    function __construct($modelData)
     {
-        global $myModel;
-        $myModel = new Model("mohamed","mohamed","airlinedatabase");
+        $myModel = new Model($modelData['userName'],$modelData['password'],$modelData['dbName']);
+
+        $postUserName = $_POST['userName'];
+        $postPassword = $_POST['password'];
+        $myModel->insert($postUserName,$postPassword);
+        $myModel->updateSeatState('purchased',$postUserName,1,'a');
+        $result = $myModel->select('SELECT * FROM airlinedatabase.Seats');
+        while($row = mysqli_fetch_assoc($result)){
+            echo "<br>";
+            echo $row['seatRow']." ".$row['seatColumn']." ".$row['seatState']." ".$row['holdingUser'];
+
+        }
+        echo "<br>";
+        $result = $myModel->select('SELECT * FROM airlinedatabase.Users');
+        while($row = mysqli_fetch_assoc($result)){
+            echo "<br>";
+            echo $row['userID']." ".$row['userPassword'];
+
+        }
+        echo "<br>";
     }
 
-
-
-}*/
+}
+$modelData=array("userName"=>"mohamed", "password"=>"mohamed", "dbName"=>"airlinedatabase");
+$myController = new Controller($modelData);
 ?>
