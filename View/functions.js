d7bc9bbb9ce59eab6a4fddc878a286aa806229d4
Mohamed Mamdouh <33672420+MohamedMamdouh95@users.noreply.github.com>
2019-06-05 14:55:25 +0200

    added css styles to the disabled checkbox and added functions.js file

---
+++ View/functions.js
@@ -0,0 +1,8 @@
+function showLoginForm() {
+    document.getElementById("formSignUp").style.display="none";
+    document.getElementById("formLogin").style.display="block";
+}
+function showSignUpForm() {
+    document.getElementById("formSignUp").style.display="block";
+    document.getElementById("formLogin").style.display="none";
+}
