09556c9a95a89bb7b2ed2c24c6ac1bd926b6c240
Mohamed Mamdouh <33672420+MohamedMamdouh95@users.noreply.github.com>
2019-06-08 21:35:46 +0200

    adding some styles and optimizing the controllerHandler

--- Controller/controllerHandler.php
+++ Controller/controllerHandler.php
@@ -1,5 +1,31 @@
 <?php
+
+
 require_once 'Controller.php';
+
+$modelData=array("userName"=>"mohamed", "password"=>"mohamed", "dbName"=>"airlinedatabase");
+$myController = new Controller($modelData);
+
+$functionsCorrespondingToRequest=array(
+    'signUpRequest'=>'signup',
+    'loginRequest'=>'Login',
+    'logoOutRequest'=>'logout',
+    'checkSeatState'=>'checkSelectedSeat',
+    'purchaseSeatRequest'=>'purchaseSeat',
+    'updateView'=>'updateView'
+);
+
+foreach( $functionsCorrespondingToRequest as $request => $function ) {
+    if(isset($_POST[$request])){
+        $result = $myController->$function();
+        unset($_POST);
+        echo $result;
+        exit();
+    }
+}
+
+
+/*require_once 'Controller.php';
 
 $modelData=array("userName"=>"mohamed", "password"=>"mohamed", "dbName"=>"airlinedatabase");
 $myController = new Controller($modelData);
@@ -43,3 +69,6 @@ if(isset($_POST['updateView'])){
     echo $updateResult;
     exit();
 }
+
+*/
+?>
